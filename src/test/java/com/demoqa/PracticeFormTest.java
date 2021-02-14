5f6f5ce66b9f269cc3aff879d16d10052415cc33
Gorbatenko Vitaliy <gorvit.dp@gmail.com>
2021-02-15 00:29:12 +0200

    add randomizer & faker

--- src/test/java/com/demoqa/PracticeFormTest.java
+++ src/test/java/com/demoqa/PracticeFormTest.java
@@ -1,17 +1,20 @@
 package com.demoqa;
 
-import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.ElementsCollection;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
+import java.text.DateFormatSymbols;
+import java.util.Calendar;
+import java.util.Locale;
 
-import static com.codeborne.selenide.CollectionCondition.*;
+import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static utils.Randomizer.randomDate;
 
 public class PracticeFormTest {
     @BeforeEach
@@ -19,23 +22,39 @@ public class PracticeFormTest {
         Configuration.browserSize = "1540x1080";
     }
 
+    private Faker faker = new Faker();
+    private String firstName;
+    private String lastName;
+    private String userEmail;
+    private String gender;
+    private String userNumber;
+    private String dayOfBirth;
+    private String monthOfBirth;
+    private String yearOfBirth;
+    private String subjectOne;
+    private String subjectTwo;
+    private String picture;
+    private String currentAddress;
+    private String state;
+    private String city;
+
     @Test
     void checkStudentRegistrationForm() {
-        //arrange
-        String firstName = "Anatolii";
-        String lastName = "Wasserman";
-        String userEmail = "Vasserman@anatolij.gov";
-        String gender = "Male";
-        String userNumber = "0661350321";
-        String dayOfBirth = "19";
-        String monthOfBirth = "December";
-        String yearOfBirth = "1952";
-        String subjectOne = "Physics";
-        String subjectTwo = "History";
-        String picture = "photo.jpg";
-        String currentAddress = "New Zealand, Shire";
-        String state = "Rajasthan";
-        String city = "Jaipur";
+        Calendar date = randomDate(-2208985139000L);
+        firstName = faker.name().firstName();
+        lastName = faker.name().lastName();
+        userEmail = faker.internet().emailAddress();
+        gender = "Male";
+        userNumber = faker.phoneNumber().subscriberNumber(10);
+        dayOfBirth = String.valueOf(date.get(Calendar.DAY_OF_MONTH));
+        monthOfBirth = String.valueOf(new DateFormatSymbols(Locale.ENGLISH).getMonths()[date.get(Calendar.MONTH)-1]);
+        yearOfBirth = String.valueOf(date.get(Calendar.YEAR));
+        subjectOne = "Physics";
+        subjectTwo = "History";
+        picture = "photo.jpg";
+        currentAddress = faker.address().streetAddress();
+        state = "Rajasthan";
+        city = "Jaipur";
 
         //act
         open("https://demoqa.com/automation-practice-form");
@@ -81,9 +100,10 @@ public class PracticeFormTest {
         elements.filterBy(text("Date of Birth")).shouldHave(texts(dayOfBirth + " " + monthOfBirth + "," + yearOfBirth));
         elements.filterBy(text("Student Email")).shouldHave(texts(userEmail));
         elements.filterBy(text("Subjects")).shouldHave(texts(subjectOne), texts(subjectTwo));
-        elements.filterBy(text("Hobbies")).shouldHave(texts("Sports"),texts("Reading"),texts("Music"));
+        elements.filterBy(text("Hobbies")).shouldHave(texts("Sports"), texts("Reading"), texts("Music"));
         elements.filterBy(text("Picture")).shouldHave(texts(picture));
         elements.filterBy(text("Address")).shouldHave(texts(currentAddress));
         elements.filterBy(text("State and City")).shouldHave(texts(state + " " + city));
     }
+
 }
